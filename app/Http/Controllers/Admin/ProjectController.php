41e31e9ad3daf4da49ae08889b7464edb8f8f161
Gulshan31 <k2caher@gmail.com>
2020-12-12 13:51:57 +0530

    work on API

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\ProjectInfo;
+use App\Models\Clients;
+use App\Models\MainServices;
 class ProjectController extends Controller
 {
     public $data;
@@ -17,10 +20,20 @@ class ProjectController extends Controller
         return view('admin.project')->with($this->data);
     }
     public function add(){
+        $this->data['clients'] = Clients::get();
+        $this->data['services'] = MainServices::get();
         return view('admin.add')->with($this->data);
     }
-    public function store()
+    public function store(Request $request)
     {
-        # code...
+        $validator = Validator::make($request->all(), [
+           'client_name'=>'required',
+           'main_service'=>'required',
+           'main_section'=>'required',
+           'sub_section'=>'required'
+        ]);
+        if ($validator->fails()) { 
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
     }
 }
--- resources/views/admin/add.blade.php
+++ resources/views/admin/add.blade.php
@@ -27,54 +27,76 @@
     @endif
     <div class="card theme_set">
         <div class="card-body">
-            <form class="row">
+            <form class="row" action="{{ route('create-project') }}" method="post" enctype="multipart/form-data">
+                @csrf
                 <div class="form-group col-sm-6">
                     <label for="client">Select Client:</label>
-                    <select class="form-control" id="client">
-                        <option>Cristo homes</option>
-                        <option>Test</option>
+                    <select class="form-control @error('client_name') is-invalid @enderror" id="client" name="client_name" onchange="generateProjectCode(this.value)" value="{{ old('client_name') }}">
+                        <option value="">Select Client</option>
+                        @foreach ($clients as $client)
+                        <option value="{{$client->id}}-{{$client->client_code}}-{{$client->no_of_projects}}">{{$client->name}}</option>
+                        @endforeach
                     </select>
+                    @error('client_name')
+                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="pb-1 form-group col-sm-6">
                     <label for="project-code"> Project Code:</label>
-                    <input class="form-control" value="CRH-001" type="text" placeholder="Project Code" id="project-code" readonly>
+                    <input class="form-control" required value="" type="text" placeholder="auto generated when you select client" id="project-code" readonly>
                 </div>
                 <div class="form-group col-sm-6">
                     <label for="main-services">Select Main Service:</label>
-                    <select class="form-control" id="main-services">
-                        <option>3D Renderings</option>
+                    <select class="form-control @error('main_service') is-invalid @enderror" id="main-services" name="main_service" value="{{ old('main_service') }}" onchange="loadMainSection(this.value)">
+                        <option value="">Select Service</option>
+                        @foreach($services as $service)
+                            <option value="{{$service->id}}">{{$service->name}}</option>
+                        @endforeach    
                     </select>
+                    @error('main_service')
+                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="form-group col-sm-6">
                     <label for="main-section">Select Main Section:</label>
-                    <select class="form-control" id="main-section">
+                    <select class="form-control @error('main_section') is-invalid @enderror" id="main-section" name="main_section" value="{{ old('main_section') }}" onchange="loadSubSection(this.value)">
                         <option>3D Renderings</option>
                     </select>
+                    @error('main_section')
+                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="form-group col-sm-6">
                     <label for="main-section">Select Sub Section:</label>
-                    <select class="form-control" id="sub-section">
+                    <select class="form-control @error('sub_section') is-invalid @enderror" id="sub-section" name="sub_section" onchange="loadListAndQuality()">
                         <option>3D Renderings</option>
                     </select>
+                    @error('sub_section')
+                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="form-group col-sm-6">
                     <label for="main-section">Select List Of Quality:</label>
-                    <select class="form-control" id="sub-section">
+                    <select class="form-control" id="quality-list">
                         <option>3D Renderings</option>
                     </select>
                 </div>
                 <div class="form-group col-sm-6">
                     <label for="main-section">Select List Of Options:</label>
-                    <select class="form-control" id="sub-section">
+                    <select class="form-control" id="option-list">
                         <option>3D Renderings</option>
                     </select>
                 </div>
                 <div class="pb-1 form-group col-sm-6">
+                    <label> Number of Renderings:</label>
+                    <input class="form-control" name="renders" value="" type="number">
+                </div>
+                <div class="pb-1 form-group col-12">
                     <label> Google Drive Link:</label>
-                    <input class="form-control" value="" type="text">
+                    <input class="form-control" name="g_drive" value="" type="text">
                 </div>
                 <div class="form-group col-12">
-                    <input class="form-control" value="" type="file" multiple placeholder="Drive link if any">
+                    <input class="form-control" value="" type="file" accept="image/*" multiple>
                 </div>
                 <div class="p-1">
                     <button type="submit" class="btn btn-dark">Submit</button>
@@ -83,5 +105,25 @@
         </div>
     </div>
 </div>
+<script>
+    function generateProjectCode(context){
+        let c = context.split('-')
+        // First Id, Client Code, Number of projects
+        // Generate project code
+        let id = Number(c[0])
+        let clientCode = c[1]
+        let numberOfProjects = Number(c[2])+1;
+        let projectCode = clientCode+'-00'+numberOfProjects
+        document.getElementById('project-code').value = projectCode;
+    }
+    function loadMainSection(){
 
+    }
+    function loadSubSection(){
+
+    }
+    function loadListAndQuality(){
+        
+    }
+</script>
 @endsection
